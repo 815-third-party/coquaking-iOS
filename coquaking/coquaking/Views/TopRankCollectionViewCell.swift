18b3713208ddc20f1ad653bdc80855731b06630c
cmindy <minjidev@naver.com>
2019-08-14 21:49:09 +0900

    feat: Create classes

---
+++ coquaking/coquaking/Views/TopRankCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TopRankCollectionViewCell.swift
+//  coquaking
+//
+//  Created by CHOMINJI on 14/08/2019.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class TopRankCollectionViewCell: UICollectionViewCell {
+    
+}
