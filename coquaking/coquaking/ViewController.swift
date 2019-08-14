d1c6d393c13beb244c04f33d57cc5248578ca3f6
cmindy <minjidev@naver.com>
2019-08-14 21:28:31 +0900

    chore: Add initial project

---
+++ coquaking/coquaking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coquaking
+//
+//  Created by CHOMINJI on 14/08/2019.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
